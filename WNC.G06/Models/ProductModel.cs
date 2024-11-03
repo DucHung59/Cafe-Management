562037e3c0e121cd3778656a209092c9e46372f3
Hung <nguyenduchung05092003@gmail.com>
2024-11-03 21:41:04 +0700

    Add project files.

---
+++ WNC.G06/Models/ProductModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WNC.G06.Models
+{
+    [Table("tblProducts")]
+    public class ProductModel
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ProductID { get; set; }
+        [Required]
+        public string ProductName { get; set; }
+        [Required]
+        public float Price { get; set; }
+        [Column("imgUrl", TypeName = "varchar(max)"), Required]
+        public string imgUrl {get; set;}
+        [Column("Description", TypeName = "varchar(max)"), Required]
+        public string Description { get; set; }
+        [Required]
+        public Boolean Status { get; set; }
+        [Required]
+        public int CafeID { get; set; }
+
+        [ForeignKey("CafeID")]
+        public CafeModel Cafe { get; set; }
+        public ICollection<OrderDetailModel> OrderDetails { get; set; } = new List<OrderDetailModel>();
+
+    }
+}
