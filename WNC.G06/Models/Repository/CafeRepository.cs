5bbb6541c4c6724034003fe9da79aa0eb4b666e7
thanhluong03 <nguyenthanhluonghh9@gmail.com>
2024-11-16 21:28:37 +0700

    Chức năng thêm sản phẩm, thêm cửa hàng

---
+++ WNC.G06/Models/Repository/CafeRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace WNC.G06.Models.Repository
+{
+    public class CafeRepository
+    {
+        private readonly DataContext _context;
+
+        public CafeRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<CafeModel> GetCafesByUserId(int userId)
+        {
+            return _context.Cafes.Where(c => c.UserID == userId && c.Status).ToList();
+        }
+
+        public bool IsCafenameExist(string cafename)
+        {
+            return _context.Cafes.Any(u => u.CafeName == cafename);
+        }
+
+        public async Task AddCafeAsync(CafeModel cafe)
+        {
+            await _context.Cafes.AddAsync(cafe);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
