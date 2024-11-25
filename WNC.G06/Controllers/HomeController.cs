428a62af017f3c4c7f1b499964a941c53f732141
thanhluong03 <nguyenthanhluonghh9@gmail.com>
2024-11-25 20:47:47 +0700

    Update

--- WNC.G06/Controllers/HomeController.cs
+++ WNC.G06/Controllers/HomeController.cs
@@ -39,9 +39,5 @@ namespace WNC.G06.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-
-        public IActionResult test() {
-            return View();
-        }
     }
 }
